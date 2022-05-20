49711d4709db8d16a555eca86b51895c951896e1
Thiago Rodrigues <hey@thiagorodrigues.dev>
2022-05-20 18:23:31 -0300

    chore: create project

---
+++ src/main/kotlin/dev/trodrigues/algalogapi/AlgalogApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.trodrigues.algalogapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlgalogApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<AlgalogApiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/trodrigues/algalogapi/AlgalogApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.trodrigues.algalogapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlgalogApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
