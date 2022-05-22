de5f418157fa2dcf0f924c4df0f85a4c9e00ddb0
Thiago Rodrigues <hey@thiagorodrigues.dev>
2022-05-21 23:23:53 -0300

    feat: add findByClientId

--- src/main/kotlin/dev/trodrigues/algalogapi/api/controllers/ClientController.kt
+++ src/main/kotlin/dev/trodrigues/algalogapi/api/controllers/ClientController.kt
@@ -2,8 +2,11 @@ package dev.trodrigues.algalogapi.api.controllers
 
 import dev.trodrigues.algalogapi.domain.entities.Client
 import dev.trodrigues.algalogapi.infra.repositories.ClientRepository
+import org.hibernate.ObjectNotFoundException
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -16,5 +19,9 @@ class ClientController(
 
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
     fun list(): List<Client> = clientRepository.findAll()
+
+    @GetMapping("/{clientId}")
+    fun findByClientId(@PathVariable clientId: UUID): ResponseEntity<Client> =
+        clientRepository.findById(clientId).map { ResponseEntity.ok(it) }.orElseThrow { IllegalArgumentException("Client not found: $clientId") }
 
 }
