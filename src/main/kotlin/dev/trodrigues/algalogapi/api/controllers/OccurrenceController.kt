7fc5587e09c61a4519f03a2a0e2d63ca28954820
Thiago Rodrigues <hey@thiagorodrigues.dev>
2022-05-23 01:02:44 -0300

    feat: add list of occurrences by delivery

--- src/main/kotlin/dev/trodrigues/algalogapi/api/controllers/OccurrenceController.kt
+++ src/main/kotlin/dev/trodrigues/algalogapi/api/controllers/OccurrenceController.kt
@@ -3,6 +3,7 @@ package dev.trodrigues.algalogapi.api.controllers
 import dev.trodrigues.algalogapi.api.requests.OccurrenceRequest
 import dev.trodrigues.algalogapi.api.responses.OccurrenceResponse
 import dev.trodrigues.algalogapi.api.responses.mapper.toResponse
+import dev.trodrigues.algalogapi.domain.services.FindDeliveryService
 import dev.trodrigues.algalogapi.domain.services.OccurrenceRecordService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -12,7 +13,8 @@ import javax.validation.Valid
 @RestController
 @RequestMapping("/delivery/{deliveryId}/occurrences")
 class OccurrenceController(
-    private val occurrenceRecordService: OccurrenceRecordService
+    private val occurrenceRecordService: OccurrenceRecordService,
+    private val findDeliveryService: FindDeliveryService
 ) {
 
     @PostMapping
@@ -23,6 +25,12 @@ class OccurrenceController(
     ): OccurrenceResponse {
         val occurrence = occurrenceRecordService.record(deliveryId, occurrenceRequest.description)
         return occurrence.toResponse()
+    }
+
+    @GetMapping
+    fun list(@PathVariable deliveryId: UUID): List<OccurrenceResponse> {
+        val delivery = findDeliveryService.findDeliveryById(deliveryId)
+        return delivery.occurrences.map { it.toResponse() }
     }
 
 }
