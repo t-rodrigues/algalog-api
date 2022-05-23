fb8dd12cbcf08e7fc166a9715a18b26fd2bea795
Thiago Rodrigues <hey@thiagorodrigues.dev>
2022-05-22 22:32:47 -0300

    feat: add findByDeliveryId

--- src/main/kotlin/dev/trodrigues/algalogapi/api/controllers/DeliveryController.kt
+++ src/main/kotlin/dev/trodrigues/algalogapi/api/controllers/DeliveryController.kt
@@ -3,18 +3,19 @@ package dev.trodrigues.algalogapi.api.controllers
 import dev.trodrigues.algalogapi.api.requests.DeliveryRequest
 import dev.trodrigues.algalogapi.domain.entities.Delivery
 import dev.trodrigues.algalogapi.domain.services.DeliveryRequestService
+import dev.trodrigues.algalogapi.domain.services.exceptions.NotFoundException
 import dev.trodrigues.algalogapi.infra.repositories.DeliveryRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import java.util.*
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/delivery")
 class DeliveryController(
-    private val deliveryRequestService: DeliveryRequestService,
-    private val deliveryRepository: DeliveryRepository
+    private val deliveryRequestService: DeliveryRequestService, private val deliveryRepository: DeliveryRepository
 ) {
 
     @PostMapping
@@ -23,7 +24,10 @@ class DeliveryController(
         deliveryRequestService.request(deliveryRequest)
 
     @GetMapping
-    fun list(pageable: Pageable): Page<Delivery> =
-        deliveryRepository.findAll(pageable)
+    fun list(pageable: Pageable): Page<Delivery> = deliveryRepository.findAll(pageable)
+
+    @GetMapping("/{deliveryId}")
+    fun findByDeliveryId(@PathVariable deliveryId: UUID): Delivery =
+        deliveryRepository.findById(deliveryId).orElseThrow { NotFoundException("delivery not found: $deliveryId") }
 
 }
